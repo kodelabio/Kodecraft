496c7ee7138168bb61ba2b5a94677724644ab2ed
Aden Eley <aden@kodelab.io>
2025-07-23 17:41:30 +0100

    vsn update

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 const settings = {
-    "minecraft_version": "1.21.4", // supports up to 1.21.1
+    "minecraft_version": "1.21.1", // supports up to 1.21.1
     "host": "minecraft-250712.kodelab.dev", 
     "port": 25565,
     "auth": "offline", // or "microsoft"

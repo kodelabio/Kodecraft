df3dca9d2d417471f1948a4268809d14b4b0f725
Maurizio <maurizio@kodelab.io>
2026-01-28 13:19:03 +0000

    real initializazztion logic

--- src/agent/external_api.js
+++ src/agent/external_api.js
@@ -13,7 +13,7 @@ import { switchAgentProfile } from '../utils/profile.js';
     
 
 export class ExternalAPI {
-    constructor(agent, globalRealmManager) {
+    constructor(agent,  worldInfo = null) {
         this.agent = agent;
         this.app = express();
         this.app.use(express.json());
@@ -21,7 +21,7 @@ export class ExternalAPI {
         // Initialize multi-bot manager: REMOVED, REPLLACES WITH ORCHESTRATION API
         //this.multiBotManager = new MultiBotManager(agent);
 
-        this.orchestration = new OrchestrationAPI(agent, globalRealmManager);
+        this.orchestration = new OrchestrationAPI(agent, worldInfo);
         // Make orchestration accessible from agent, this will allow us to stop workers when an agent is stopped
         this.agent.orchestration = this.orchestration; 
         // Kick workers on bot disconnect
